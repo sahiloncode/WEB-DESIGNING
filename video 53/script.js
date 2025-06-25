bdd14052aea6a8b519476f89af2374a1a24601d6
Sahil Rajput <147192706+SahilRajput47@users.noreply.github.com>
2025-06-25 21:52:19 +0530

    Js Start

---
+++ video 53/script.js
@@ -0,0 +1,12 @@
+alert("hello world")
+console.log("Code Is Loading ...")
+
+var a = prompt("Enter Your Password")
+console.log("Your Password Is: " + a)
+var isTrue = confirm("You Sure About That")
+if (isTrue) {
+  console.log("That Is A Warning")
+} else {
+  console.log("You Are Safe")
+}
+console.log("Your Number is " + a)
