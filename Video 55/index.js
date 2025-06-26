11a14a0ab0b633412d6c705b4dbee0f0ebd4ce34
Sahil Rajput <147192706+SahilRajput47@users.noreply.github.com>
2025-06-26 15:05:21 +0530

    JS If Else If Else Ladder Complete

---
+++ Video 55/index.js
@@ -0,0 +1,51 @@
+// console.log("Hello Its A Contodional Chapter")
+// let age =17;
+// let grace =2;
+// age += grace;
+// console.log(age);
+// console.log(age + grace);
+// console.log(age - grace);
+// console.log(age * grace);
+// console.log(age / grace);
+// console.log(age ** grace);
+// console.log(age % grace);
+
+// if((age+grace)>18){
+//   console.log("You Can Drive");
+// }
+// else{
+//   console.log("You Can't Drvive");
+// }
+
+// let age=12;
+// let age2=18;
+// if(age,age2==18){
+//   console.log("You Can Drive")
+// }
+// else{
+//   console.log("You Can't Drive");
+// }
+
+age =10;
+if(age==18){
+  console.log("You Can Drive");
+}
+else if(age==0){
+   console.log("Enter The Right Age");
+}
+else{
+  console.log("You Can't Drive");
+}
+
+a=6;
+b=9;
+let c= a>b ?(a-b) :(b-a);
+console.log(c);   //Ternary Operator Use
+
+/*Explanation
+if (a>b){
+let c=a-b;
+}
+else{
+  let c= b-a
+} */
