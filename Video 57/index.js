ea61ededc294f363e99d89cb0ce23a0d7c61a7b1
Sahil Rajput <147192706+SahilRajput47@users.noreply.github.com>
2025-06-30 12:54:09 +0530

    fuctions complete

---
+++ Video 57/index.js
@@ -0,0 +1,24 @@
+// function change(name) {
+//   console.log("hey " + name + " whats up");
+//   console.log("hey " + name + " what are you doing");
+//   console.log("hey " + name + " is that everthing is ok");
+//   console.log("hey " + name + " look at tahe sky");
+// }
+// change("HAYATE")
+// change("LUFFY")
+
+// function sum(a, b){
+//   // console.log(a + b)
+//   return a + b;
+// }
+// result = sum(10, 20)
+// console.log("The Sum Of A And B Is:" + result);
+
+// arrrow function
+
+const func1 = (x)=>{
+  console.log("Hello " + x);
+}
+func1 ("HAYATE");
+func1 ("ZORO");
+func1 ("LUFFY");
