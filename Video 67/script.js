1f15b0d5c963c800ad6540eaecae0d8aa425b1bd
Sahil Rajput <147192706+SahilRajput47@users.noreply.github.com>
2025-07-09 23:11:02 +0530

    Dom Id Class Selectors Completed

---
+++ Video 67/script.js
@@ -0,0 +1,7 @@
+console.log("HAYATE");
+
+let boxes = document.getElementsByClassName("box");
+console.log(boxes);
+boxes[3].style.backgroundColor = "pink";
+
+document.getElementById("orange").style.backgroundColor = "aqua";
